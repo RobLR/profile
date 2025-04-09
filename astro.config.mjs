739bfb30f82f573a3d031dcb88f6cc805c2b2e79
RobLR <94964242+RobLR@users.noreply.github.com>
2025-04-09 18:58:05 +0100

    update configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  //   site: "",
+  //   base: "/",
+  //   trailingSlash: "never",
+  server: { open: "/" },
+});

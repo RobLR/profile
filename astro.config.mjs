5719f2c35f5397a62b094842cfb45fbd694bae19
RobLR <94964242+RobLR@users.noreply.github.com>
2025-04-23 13:35:11 +0100

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://roblr.github.io",
-  base: "/profile",
+  base: "/roblr",
   //   trailingSlash: "never",
   server: { open: "/" },
 });
